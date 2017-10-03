3b653a00a7e54db0fc77840e5938494ffc001fa8
BhupinderSF <bhupinder.garg@sourcefuse.com>
2017-10-03 18:36:20 +0530

    office changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Ng Model lives here
 
 import { AppComponent } from './default/app.component';
 
